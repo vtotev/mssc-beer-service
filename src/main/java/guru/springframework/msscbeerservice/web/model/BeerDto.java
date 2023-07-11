b06f56a490460246e167143405e469b51cdc1775
Valeri Totev <valerivt@yahoo.com>
2023-07-11 13:51:20 +0300

    added validation

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -1,5 +1,9 @@
 package guru.springframework.msscbeerservice.web.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,15 +19,33 @@ import java.util.UUID;
 @Builder
 public class BeerDto {
 
+    @Null
     private UUID id;
-    private Integer version;
-    private OffsetDateTime createdDate;
-    private OffsetDateTime lastModifiedDate;
-    private String beerName;
-    private BeerStyleEnum beerStyle;
-    private Long upc;
-    private BigDecimal price;
-    private Integer quantityOnHand;
-}
 
+    @Null
+    private Integer version;
+
+    @Null
+    private OffsetDateTime createdDate;
+
+    @Null
+    private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
+    private String beerName;
+
+    @NotNull
+    private BeerStyleEnum beerStyle;
+
+    @Positive
+    @NotNull
+    private Long upc;
+
+    @Positive
+    @NotNull
+    private BigDecimal price;
+
+    private Integer quantityOnHand;
+
+}
 
