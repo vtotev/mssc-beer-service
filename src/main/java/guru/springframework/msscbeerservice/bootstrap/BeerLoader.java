b57c9e93e8ea235c46622cfde298b0bb16dd4f91
Valeri Totev <valerivt@yahoo.com>
2024-01-15 14:22:49 +0200

    corrected hibernate error and set active profile

--- src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
@@ -24,7 +24,7 @@ public class BeerLoader  implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        loadBeerObjects();
+//        loadBeerObjects();
     }
 
     private void loadBeerObjects() {
