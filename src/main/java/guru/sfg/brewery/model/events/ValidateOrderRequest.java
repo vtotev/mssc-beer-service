ddcc2b0ce4759a9ad8f25662d76cf0bb9a6807ff
Valeri Totev <72451048+vtotev@users.noreply.github.com>
2024-02-04 19:24:21 +0200

    adding validation result listener

---
+++ src/main/java/guru/sfg/brewery/model/events/ValidateOrderRequest.java
@@ -0,0 +1,14 @@
+package guru.sfg.brewery.model.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ValidateOrderRequest {
+    private BeerOrderDto beerOrder;
+}
