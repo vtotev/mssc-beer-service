a9f0573af24ca60d8a426cc41b444b36a4e7a202
Valeri Totev <72451048+vtotev@users.noreply.github.com>
2024-02-25 18:10:58 +0200

    using hystrix circuit breaker with feign client

--- src/main/java/guru/sfg/springframework/msscbeerservice/services/inventory/InventoryServiceFeignClient.java
+++ src/main/java/guru/sfg/springframework/msscbeerservice/services/inventory/InventoryServiceFeignClient.java
@@ -1,6 +1,5 @@
 package guru.sfg.springframework.msscbeerservice.services.inventory;
 
-import guru.sfg.springframework.msscbeerservice.services.inventory.BeerInventoryServiceRestTemplateImpl;
 import guru.sfg.springframework.msscbeerservice.services.inventory.model.BeerInventoryDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
 import java.util.UUID;
 
-@FeignClient(name = "inventory-service")
+@FeignClient(name = "inventory-service", fallback = InventoryServiceFeignClientFailover.class)
 public interface InventoryServiceFeignClient {
 
     @RequestMapping(method = RequestMethod.GET, value = BeerInventoryServiceRestTemplateImpl.INVENTORY_PATH)
---
+++ src/main/java/guru/sfg/springframework/msscbeerservice/services/inventory/InventoryServiceFeignClientFailover.java
@@ -0,0 +1,21 @@
+package guru.sfg.springframework.msscbeerservice.services.inventory;
+
+import guru.sfg.springframework.msscbeerservice.services.inventory.model.BeerInventoryDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class InventoryServiceFeignClientFailover implements InventoryServiceFeignClient {
+
+    private final InventoryFailoverFeignClient failoverFeignClient;
+
+    @Override
+    public ResponseEntity<List<BeerInventoryDto>> getOnhandInventory(UUID beerId) {
+        return failoverFeignClient.getOnhandInventory();
+    }
+}
